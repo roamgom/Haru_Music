8e184ede0a967e965a1201ecde8ac5cb7543a78f
DESKTOP-LF00QQL\Gom <gomaguwi@gmail.com>
2017-05-01 20:06:45 +0900

    added Music_link DB and Editted Hello_Comment to "Hola!"

--- haru_music_main.py
+++ haru_music_main.py
@@ -1,7 +1,7 @@
 from slacker import Slacker
 import json
 
-with open ('config\config.json', 'r') as cf:
+with open ('config\config_sc.json', 'r') as cf:
     config = json.load(cf)
 
 api_key = config['CI']['api_token']
@@ -9,7 +9,7 @@ ci_hook_url = config['CI']['hook_url']
 
 slack = Slacker(api_key)
 
-slack.chat.post_message('#gom_lab', 'Hello! Testing!')
+slack.chat.post_message('#gom_lab', 'Hola!')
 
 response = slack.users.list()
 users = response.body['members']
