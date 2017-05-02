fb489706b40dffbc67041c4bf4682ba0ad64e48c
DESKTOP-LF00QQL\Gom <gomaguwi@gmail.com>
2017-05-02 13:22:48 +0900

    Now Can send message to Slack! Added Comments for sending Music to channel

--- haru_music_main.py
+++ haru_music_main.py
@@ -1,5 +1,6 @@
 from slacker import Slacker
 import json
+import random
 
 with open ('config\config_sc.json', 'r') as cf:
     config = json.load(cf)
@@ -11,12 +12,19 @@ with open ('MediaS\music_link.json', 'r') as ml:
 
 
 api_key = config['CI']['api_token']
-ci_hook_url = config['CI']['hook_url']
+ci_hook_url = config['CI']['incoming_webhook']['hook_url']
 slack = Slacker(api_key)
 # Keys for Haru_Music
-select_motion = [['Calm']]
-for selection
+
+feel_random = random.choice(list(command['Music'].keys()))
+genre_random = random.choice(list(music[feel_random].keys()))
+artist_random = random.choice(list(music[feel_random][genre_random].keys()))
+song_random = random.choice(list(music[feel_random][genre_random][artist_random].keys()))
+link_song = (music[feel_random][genre_random][artist_random][song_random])
+
+hi_comment = command['Message']['say_hi'] + command['Message']['say_music'] + '\'',feel_random, '\'' + command['Message']['say_today']
+song_info = 'Genre :', genre_random, ' #Artist :', artist_random, ' #Song :', song_random + '\n' + link_song
 
 
-slack.chat.post_message('#gom_lab', 'Hola!', username = "Haru_Music")
+slack.chat.post_message('#gom_lab', text = 'hola!', attachments = [{"pretext": ''.join(hi_comment), "text": ''.join(song_info)}], username = "Haru_Music")
 
